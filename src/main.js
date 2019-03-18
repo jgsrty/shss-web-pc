1e209d0a3cf9cefd64f30e2710dbc546f4a2218b
jgsrty <573850993@qq.com>
2019-03-18 09:48:19 +0800

    优化vuex结构

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router/index'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router/index";
+import store from "./store/index";
 import "normalize.css/normalize.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
