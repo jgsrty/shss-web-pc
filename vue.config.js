1b7907a7d5e4dad3390d62b9d3ba78c3e7f7b0cb
jgsrty <573850993@qq.com>
2019-03-18 10:20:29 +0800

    1.添加公共scss 2.修改项目测试和上线环境配置 3.配置webpack

---
+++ vue.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+module.exports = {
+  productionSourceMap: false,
+  configureWebpack: () => ({
+    resolve: {
+      alias: {
+        "@": path.resolve("./src")
+      }
+    }
+  }),
+  chainWebpack: config => {
+    // 移除 prefetch 插件
+    config.plugins.delete("prefetch");
+    config.plugin("define").tap(args => {
+      args[0]["process.env"].BASE_URL = JSON.stringify(process.env.BASE_URL);
+      return args;
+    });
+  },
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "@/style/global.scss";`
+      }
+    }
+  }
+};
