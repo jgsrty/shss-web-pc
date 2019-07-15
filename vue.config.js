f1060639a18aa2bac220a3a488474a53903223ad
RtyXmd <573850993@qq.com>
2019-07-15 17:00:32 +0800

    add gzip

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,32 @@
-const path = require("path");
+const path = require('path')
+const CompressionPlugin = require('compression-webpack-plugin')
 module.exports = {
   productionSourceMap: false,
   configureWebpack: () => ({
     resolve: {
       alias: {
-        "@": path.resolve("./src")
+        '@': path.resolve('./src')
       }
     }
   }),
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins:[new CompressionPlugin({
+          test:/\.js$|\.html$|\.css/,
+          threshold:10240,
+          deleteOriginalAssets:false
+        })]
+      }
+    }
+  },
   chainWebpack: config => {
     // 移除 prefetch 插件
-    config.plugins.delete("prefetch");
-    config.plugin("define").tap(args => {
-      args[0]["process.env"].BASE_URL = JSON.stringify(process.env.BASE_URL);
-      return args;
-    });
+    config.plugins.delete('prefetch')
+    config.plugin('define').tap(args => {
+      args[0]['process.env'].BASE_URL = JSON.stringify(process.env.BASE_URL)
+      return args
+    })
   },
   css: {
     loaderOptions: {
@@ -23,4 +35,4 @@ module.exports = {
       }
     }
   }
-};
+}
